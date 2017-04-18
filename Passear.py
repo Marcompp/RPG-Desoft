38e619b6791c1853abbdf908514ea8688bf0c4d8
RaphaelAzev <raphaelazev@hotmail.com>
2017-04-18 15:08:14 -0300

    Funções Passear e Encounter

---
+++ Passear.py
@@ -0,0 +1,39 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Apr 18 13:40:37 2017
+
+@author: Raphael
+"""
+import random as rd
+import time
+import Batalha as btl
+
+def Encount(jog,loc):
+    rend = rd.randint(0, len(loc["inimigos"]))
+    print("Você foi atacado por {0}!!!".format(loc['inimigos'][rend]))
+    btl.Comando(jog,loc['inimigos'][rend])
+
+def Passear(jog,loc,step):
+    print("Você está n{0}.".format(loc["nome"]))
+    time.sleep(1.5)
+    rand = rd.randint(0, 10)
+    if rand <= loc["encounter"]:
+        Encount(jog,loc)
+        step+=1
+    elif rand == loc['loot']:
+        a = input("Você encontrou um tesouro, deseja abrir?(S ou N?) ")
+        if a == 'S':
+            randi = rd.randint(0, len[loc]['treasure'])
+            print("Você achou {0}!".format(loc['treasure'][randi]))
+    if step == 0:
+        print("Ainda há um grande caminho a percorrer./nVocê anda um pouco em direção a seu próximo objetivo")
+        step = 1
+        return step
+    elif step == 1:
+        print("Você sente já estar no meio do caminho, com isso em mente você continua seu caminho sossegado")
+        step = 2
+        return step
+    elif step == 2:
+        print("Já é possivel ver a próxima área, falta pouco para sair desse local!/n Você sente uma presença maligna olhando para você")
+        step = 3
+        return step
