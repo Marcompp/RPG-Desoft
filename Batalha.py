68997ff6ffd2aa2933ae0c4371f3feeb35850ba0
marco moliterno <marco moliterno>
2017-04-24 19:23:05 -0300

    aa

--- Batalha.py
+++ Batalha.py
@@ -16,6 +16,20 @@ with open ("Weapons.json") as wpns:
 with open ("Techs.json") as tec:
     Tech = json.load(tec)
 
+def poison(A,B):
+    if A["status"] == "poison":
+        time.sleep(1)
+        dmg = int(A["HP"]/10)
+        print("{0} is hurt by poison! {1} damage!".format(A["nome"],dmg))
+        A["HP"] -= dmg
+    if B["status"] == "poison":
+        dmg = int(B["HP"]/10)
+        time.sleep(1)
+        print("{0} is hurt by poison! {1} damage!".format(B["nome"],dmg))
+        B["HP"] -= dmg
+    return (A,B)
+    
+    
 def attack(A,B):
     print("{0} attacks with {1}!".format(A["nome"],A["Attack"]["nome"]))
     NHP = B["HP"]
@@ -29,6 +43,7 @@ def attack(A,B):
     critA = WeaponA["crit"]+A["skl"]/2- B["lck"]
     print("Hit={0} Dmg={1} Crit={2}".format(hitA,attackA,critA))
     truehit = (random.randint(1,100) + random.randint(1,100))/2
+    eff = 'none'
     if truehit <= hitA:
         print("The attack hit! ",end="")
         if random.randint(1,100)<= critA:
@@ -37,13 +52,16 @@ def attack(A,B):
         else:
             print("{} damage!".format(attackA))
             NHP = B["HP"] - attackA
+        if WeaponA["effect"] == "poison":
+            print("{0} was poisoned!".format(B["nome"]))
+            eff = 'poison'
         if NHP > 0:
             print("{0} has {1} hp left!".format(B["nome"],NHP))
         else:
             print("{0} died!".format(B["nome"]))
     else:
         print("The attack missed!")
-    return NHP
+    return NHP,eff
 
 #Broadsword = [11,65,10,5]
 #Poleaxe = [12,65,11,0]
@@ -65,21 +83,29 @@ def attack(A,B):
 def Turno(Agnes,Borin):
     import time
     while True:
-        Borin["HP"] = attack(Agnes,Borin)
+        Borin["HP"],eff= attack(Agnes,Borin)
         if Borin["HP"] <= 0:
             break
+        if eff == "poison":
+            Borin["status"]="poison"
         time.sleep(2) # delays for 2 seconds
-        Agnes["HP"] = attack(Borin,Agnes)
+        Agnes["HP"],eff = attack(Borin,Agnes)
         if Agnes["HP"] <= 0:
             break
+        if eff == "poison":
+            Agnes["status"]="poison"
         time.sleep(2) # delays for 2 seconds
         if Agnes["spd"]-Agnes["Wpn"]["wgt"] -(Borin["spd"] - Borin["Wpn"]["wgt"]) >= 3:
-            Borin["HP"] = attack(Agnes,Borin)
+            Borin["HP"],eff = attack(Agnes,Borin)
+            if eff == "poison":
+                Borin["status"]="poison"
         elif Borin["spd"]-Borin["Wpn"]["wgt"] - (Agnes["spd"] - Agnes["Wpn"]["wgt"]) >= 3:
-            Agnes["HP"] = attack(Borin,Agnes)    
+            Agnes["HP"],eff = attack(Borin,Agnes) 
+            if eff == "poison":
+                Agnes["status"]="poison"
         time.sleep(2) # delays for 2 seconds
         break
-    return Agnes["HP"],Borin["HP"]
+    return Agnes["HP"],Borin["HP"],Agnes["status"],Borin["status"]
 
 def Fight(Agnes,Borin):
     #enemy attack
@@ -105,6 +131,8 @@ import time
 def Comando(Agnes,Borin):
     Agnes["Wpn"] = Wpn[Agnes["Weapon"]]
     Borin["Wpn"] = Wpn[Borin["Weapon"]]
+    Agnes["status"] = "none"
+    Borin["status"] = "none"
     while Agnes["HP"] > 0 and Borin["HP"] > 0:
         os.system("cls")#ClearScreen
         print("{0} has {1} HP.".format(Agnes["nome"],Agnes["HP"]))
@@ -129,7 +157,7 @@ def Comando(Agnes,Borin):
                 print("-Attack (Mgt={0} Acc={1} Wgt={2} Crit={3} Effect={4})".format(Agnes["Wpn"]["mgt"],Agnes["Wpn"]["acc"],Agnes["Wpn"]["wgt"],Agnes["Wpn"]["crit"],Agnes["Wpn"]["effect"]))
                 for a in range(0,len(Agnes["Techs"])):
                     time.sleep(0.5)
-                    print("-{0} (Mgt={1} Acc={2} Wgt={3} Crit={4})".format(Tech[Agnes["Techs"][a]]["nome"],Tech[Agnes["Techs"][a]]["mgt"],Tech[Agnes["Techs"][a]]["acc"],Tech[Agnes["Techs"][a]]["wgt"],Tech[Agnes["Techs"][a]]["crit"],Tech[Agnes["Techs"][a]]["effect"]))
+                    print("-{0} (Mgt={1} Acc={2} Wgt={3} Crit={4} Effect={5})".format(Tech[Agnes["Techs"][a]]["nome"],Tech[Agnes["Techs"][a]]["mgt"],Tech[Agnes["Techs"][a]]["acc"],Tech[Agnes["Techs"][a]]["wgt"],Tech[Agnes["Techs"][a]]["crit"],Tech[Agnes["Techs"][a]]["effect"]))
                 print("-Cancel")
                 move = input("")
             if move == "Cancel":
@@ -139,6 +167,7 @@ def Comando(Agnes,Borin):
             else:
                 Agnes["Attack"] = Tech[move]
             #calculo quem vai antes
+            Agnes,Borin = poison(Agnes,Borin)
             Agnes,Borin = Fight(Agnes,Borin)
         if command == 2:
             #inventorio
@@ -148,15 +177,22 @@ def Comando(Agnes,Borin):
             escape = sav.Escape(Agnes)
             if escape == True:
                 print("You escaped safely")
+                time.sleep(1)
                 return Agnes,True
             else:
                 print("You failed to escape")
+                time.sleep(1)
                 attack(Borin,Agnes)
             pass
     return Agnes,False
         
     
 def Begin(Agnes,Borin):
+    with open ("Beast.json") as bst:
+        Bst = json.load(bst)
+    if Borin["nome"] not in Bst:
+        Bst[Borin["nome"]] = Borin
+        sav.SaveGameB(Bst)
     print("Suddenly, {} attacks you!".format(Borin["nome"]))
     time.sleep(2)
     Agnes["HP"] =Agnes["MHP"]
@@ -169,5 +205,5 @@ def Begin(Agnes,Borin):
         sav.SaveGameP(Agnes)
     else:
         print("YOU LOSE")
-        time.sleep(4)
-        os.sys.exit
+        time.sleep(0.5)
+        os._exit(0)
