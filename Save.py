625c44a2b8414773b7935a07b7aaeff83fb6d9b4
RaphaelAzev <raphaelazev@hotmail.com>
2017-04-24 14:51:33 -0300

    Update with LvlUp Function

--- Save.py
+++ Save.py
@@ -6,6 +6,8 @@ Created on Tue Apr 18 15:17:00 2017
 """
 
 import json
+import random as rd
+import time
 
 
 def SaveGameP(PD):
@@ -39,6 +41,31 @@ def SaveAll(PD,BD,ID,AD):
     SaveGameI(ID)
     SaveGameA(AD)
 
+def XpCount(P,E):
+    EXPgain = E["lvl"]/P["lvl"] * 100
+    print("You got {0} XP!".format(EXPgain))
+    time.sleep(2.5)
+    P["xp"] += EXPgain
+    if P["xp"] >= 100:
+        print("You leveled up!!!")
+        time.sleep(2.5)
+        P = LvlUp(P)
+    return P
+
+def LvlUp(P):
+    P["xp"] = 0
+    P["lvl"] += 1
+    Stats = ["MHP","atk","skl","spd","lck","def"]
+    Growths = [80,30,40,35,50,25]
+    for a in range(len(Stats)):
+        count = rd.randint(0, 100)
+        if count <= Growths[a]:
+            P[Stats[a]] += 1
+            time.sleep(1)
+            print("{0} went up by 1!".format((Stats[a])))
+    time.sleep(1)
+    return P
+    
 
 
         
