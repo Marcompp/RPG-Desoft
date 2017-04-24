97de758a6451386fe3466838b24b0ab9bd7eba6d
marco moliterno <marco moliterno>
2017-04-24 13:43:09 -0300

    ad

--- Start.py
+++ Start.py
@@ -8,6 +8,8 @@ Created on Tue Apr 18 15:02:00 2017
 import json
 import Batalha as blt
 import Passear as psa
+import Misc as misc
+import Save as sav
 import os
 import time
 
@@ -20,19 +22,20 @@ with open ("Weapons.json") as wpns:
 with open ("Locations.json") as loca:
     Loca = json.load(loca)
 
+with open ("Player.json") as play:
+    player = json.load(play)
 
+if player == {"Start":0}:
+    player = misc.NewGame()
+player["HP"] = player["MHP"]
+sav.SaveGameP(player)
 
-
-
-
-player = Char["Agnes"]
 location = Loca["Apple Woods"]
-player["Weapon"] = Wpn["Broadsword"]
 step = 3
 
 #save stuff
 os.system("cls")#ClearScreen
-print("What will you do?")
+#print("What will you do?")
 options = ["1)Adventure","2)Train","3)Sleep","4)Bestiary"]
 command="aaaa"
 while command not in range(1,5):
